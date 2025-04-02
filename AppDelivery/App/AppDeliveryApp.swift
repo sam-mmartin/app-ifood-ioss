930a1ddca9f66e4a891676d880a3274be325da1d
Sam Martin <sammartin@192.168.0.5>
2025-04-02 10:44:22 -0300

    Implementado home-view e animacao de texto

--- AppDelivery/App/AppDeliveryApp.swift
+++ AppDelivery/App/AppDeliveryApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct AppDeliveryApp: App {
   var body: some Scene {
     WindowGroup {
-      ContentView()
+      HomeView()
     }
   }
 }
---
+++ AppDelivery/HomeView/HomeView.swift
@@ -0,0 +1,38 @@
+import SwiftUI
+
+struct HomeView: View {
+
+  @State private var isAnimating = false
+
+  var body: some View {
+    VStack {
+      Text("App Delivery")
+        .font(.system(size: 40))
+        .fontWeight(.heavy)
+        .foregroundColor(Color("ColorRed"))
+        .opacity(isAnimating ? 1 : 0)
+        .offset(y: isAnimating ? 0 : -40)
+      
+      Text("Peça as suas comidas no conforto da sua casa")
+        .font(.title2)
+        .padding()
+        .multilineTextAlignment(.center)
+        .foregroundColor(.black.opacity(0.7))
+        .opacity(isAnimating ? 1 : 0)
+        .offset(y: isAnimating ? 0 : -40)
+
+      Spacer()
+    }
+    .onAppear {
+      withAnimation(.easeInOut(duration: 1)) {
+        isAnimating = true
+      }
+    }
+    // Final VStack
+
+  }  // Final View
+}  // Final struct
+
+#Preview {
+  HomeView()
+}
